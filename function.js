05f202470b03072e3413ee2810677a2b636acac6
nemenlah freeman <freeerzuah94@gmial.com>
2023-11-20 13:17:37 +0000

    all

--- function.js
+++ function.js
@@ -42,6 +42,9 @@ xBtn.addEventListener("click", function() {
    
 });
 
+
+//   mobile view
+if (window.innerWidth < 500) {
 Playagain.addEventListener("click", function() {
     document.querySelector(".box1").style.display = "none";
     document.getElementById("tag").style.display = "block";
@@ -64,12 +67,163 @@ Playagain3.addEventListener("click", function() {
     document.querySelector("#myRules").style.left="90%";
 });
 
+    
+    hAnd.addEventListener('click', () => {
+        document.querySelector(".box1").style.display = "block";
+        document.getElementById("tag").style.display = "none";
+
+    document.getElementById("Bonus").style.display="none"
+    document.querySelector("#myRules").style.left="10rem";
+    
+    const Aichoice = document.getElementById("Aichoice1");
+    let Hand = Math.ceil(Math.random()*3);
+
+    if (Hand > 0 && Hand < 2) {
+        Aichoice.src = "img/rock.svg";
+        result.innerHTML = "You Win";
+        wincount++;
+        win.innerHTML = wincount;
+        oval2.style.display = "none";
+
+        setTimeout(function() {
+            oval.style.display = "flex";
+        }, 1000);
+    } else if (Hand > 1 && Hand < 3) {
+        Aichoice.src = "img/hand.svg";
+        result.innerHTML = "Tie";
+        oval.style.display = "none";
+        oval2.style.display = "none";
+    } else if (Hand > 2 && Hand < 4) {
+        Aichoice.src = "img/hand2.svg";
+        result.innerHTML = "You Lose";
+
+        setTimeout(function() {
+            oval2.style.display = "flex";
+        }, 1000);
+        
+        oval.style.display = "none";
+
+        if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+    }
+});
+
+hAnd2.addEventListener('click', () => {
+    document.querySelector(".box2").style.display = "block";
+    document.getElementById("tag").style.display = "none";
+    document.getElementById("Bonus").style.display="none"
+    document.querySelector("#myRules").style.left="50%";
+    
+    const Aichoice = document.getElementById("Aichoice2");
+    let Hand2 = Math.ceil(Math.random()*3);
+
+    if (Hand2 > 0 && Hand2 < 2) {
+        Aichoice.src = "img/rock.svg";
+        result2.innerHTML = "You Lose";
+        
+        setTimeout(function() {
+            oval4.style.display = "flex";
+        }, 1000);
+
+        oval3.style.display = "none";
+
+        if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+    } else if (Hand2 > 1 && Hand2 < 3) {
+        result2.innerHTML = "You Win";
+        wincount++;
+        win.innerHTML = wincount;
+        Aichoice.src = "img/hand.svg";
+        oval4.style.display = "none";
+
+        setTimeout(function() {
+            oval3.style.display = "flex";
+        }, 1000);
+    } else if (Hand2 >= 2 && Hand2 < 4) {
+        result2.innerHTML = "Tie";
+        Aichoice.src = "img/hand2.svg";
+        oval3.style.display = "none";
+        oval4.style.display = "none";
+    }
+});
+
+rOck.addEventListener('click', () => {
+    document.querySelector(".box3").style.display = "block";
+    document.getElementById("tag").style.display = "none";
+    document.getElementById("Bonus").style.display="none";
+    document.querySelector("#myRules").style.left="50%";
+
+    const Aichoice = document.getElementById("Aichoice3");
+    let rOck = Math.ceil(Math.random()*3);
+
+    if (rOck > 0 && rOck < 2) {
+        Aichoice.src = "img/rock.svg";
+        result3.innerHTML = "Tie";
+        oval5.style.display = "none";
+        oval6.style.display = "none";
+    } else if (rOck > 1 && rOck < 3) {
+        Aichoice.src = "img/hand.svg";
+        result3.innerHTML = "You Lose";
+
+        setTimeout(function() {
+            oval6.style.display = "flex";
+        }, 1000);
+
+        oval5.style.display = "none";
+
+        if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+    } else if (rOck > 2 && rOck < 4) {
+        Aichoice.src = "img/hand2.svg";
+        result3.innerHTML = "You Win";
+        oval6.style.display = "none";
+
+        setTimeout(function() {
+            oval5.style.display = "flex";
+        }, 1000);
+        
+        wincount++;
+        win.innerHTML = wincount;
+    }
+});
+}
+
+// desktop view
+Playagain.addEventListener("click", function() {
+    document.querySelector(".box1").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    document.getElementById("Bonus").style.display = "block";
+    // document.querySelector("#myRules").style.left="90%";
+
+});
+
+Playagain2.addEventListener("click", function() {
+    document.querySelector(".box2").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    document.getElementById("Bonus").style.display = "block";
+    // document.querySelector("#myRules").style.left="90%";
+});
+
+Playagain3.addEventListener("click", function() {
+    document.querySelector(".box3").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    document.getElementById("Bonus").style.display = "block";
+    // document.querySelector("#myRules").style.left="90%";
+});
+
+
 
 hAnd.addEventListener('click', () => {
     document.querySelector(".box1").style.display = "block";
     document.getElementById("tag").style.display = "none";
     document.getElementById("Bonus").style.display="none"
-    document.querySelector("#myRules").style.left="10rem";
+    // document.querySelector("#myRules").style.left="10rem";
 
     const Aichoice = document.getElementById("Aichoice1");
     let Hand = Math.ceil(Math.random()*3);
@@ -110,7 +264,7 @@ hAnd2.addEventListener('click', () => {
     document.querySelector(".box2").style.display = "block";
     document.getElementById("tag").style.display = "none";
     document.getElementById("Bonus").style.display="none"
-    document.querySelector("#myRules").style.left="50%";
+    // document.querySelector("#myRules").style.left="50%";
 
     const Aichoice = document.getElementById("Aichoice2");
     let Hand2 = Math.ceil(Math.random()*3);
@@ -151,7 +305,7 @@ rOck.addEventListener('click', () => {
     document.querySelector(".box3").style.display = "block";
     document.getElementById("tag").style.display = "none";
     document.getElementById("Bonus").style.display="none";
-    document.querySelector("#myRules").style.left="50%";
+    // document.querySelector("#myRules").style.left="50%";
 
     const Aichoice = document.getElementById("Aichoice3");
     let rOck = Math.ceil(Math.random()*3);
@@ -241,94 +395,3 @@ rOck.addEventListener('click', () => {
 
 
 
-// result.innerHTML = "You Win!" ;
-// }
-// else if(Aichoice.src === "img/hand1.svg")
-// {
-//     console.log('Draw ');
-// }
-// else if(Aichoice ===  "img/hand2.svg")
-// {
-//     console.log('Lose');
-//     }
-
-
-
-
-
-
-
-        // function Home(){
-        //     window.open(" index.html")
-        // }
-
-      
-     
-
-
-
-        // function createNewDivs(data) {
-        //     const DivsContainer = document. getElementById("box1");
-        
-        //     data. forEach ((item) => {
-        //     const newDiv = document.createElement("div");
-        //     newDiv.classList.add("newDiv");
-        //     newDiv.innerHTML =`
-        //     ‹div class="logo">
-        //     <img src="img/hand.svg">
-         
-        //     </div> 
-        
-        //     </div>`
-        //     ;
-        //     DivsContainer.appendChild(newDiv);
-        //     });
-        // }
-
-
-
-
-        // hAnd2.addEventListener('click', () =>{
-
-        //     document.querySelector(".box2").style.display = "block";
-        //     document.getElementById("tag").style.display = "none";
-        
-        //     let Hand2 = Math.ceil(Math.random()*3);
-        //     let ai = '';
-        //     if (Hand2 > 0 && Hand2 < 2){
-        //       ai = 'Rock';
-        //     }
-        //  else if(Hand2 > 1 && Hand2 <3){
-        //     ai = 'Paper';
-        // }
-        //     else if(Hand2 >= 2 && Hand2 < 4){
-        //         ai = 'Scissors';
-            
-        //     }
-        //     console.log(ai)
-        //     if (ai === 'Rock')
-        //     {
-        //         console.log('Lose');
-        //     }
-        //     else if(ai === 'Paper')
-        //     {
-        //         console.log('Win! ');
-        //     }
-        //     else if(ai === 'Scissors')
-        //     {
-        //         console.log('Draw');
-        //         }
-        //     })
-        // if (ai === 'Rock')
-        // {
-        //     console.log('Draw');
-        // }
-        // else if(ai === 'Paper')
-        // {
-        //         console.log('You Lose! ');
-
-        // }
-        // else if(ai === 'Scissors')
-        // {
-        //     console.log('You Win!');
-        //     }
--- function1.js
+++ function1.js
@@ -4,6 +4,7 @@ const lizard = document.getElementById("lizard");
 const cross = document.getElementById("cross");
 const rock = document.getElementById("rock1");
 
+
 const result = document.getElementById("Result");
 const result2 = document.getElementById("Result2");
 const result3 = document.getElementById("Result3");
@@ -19,6 +20,8 @@ const Playagain8 = document.getElementById("Home5");
 
 
 const myRules = document.querySelector("#myRules");
+const OR = document.querySelector("#OR");
+const Rule = document.querySelector(".myRules1");
 const xBtn = document.querySelector(".close");
 const Popup = document.querySelector(".popup");
 
@@ -26,6 +29,8 @@ const Popup = document.querySelector(".popup");
 myRules.addEventListener('click', function() {
     Popup.style.display ="flex";
 });
+ 
+
 
 xBtn.addEventListener("click", function() {
     Popup.style.display = "none";
@@ -35,34 +40,45 @@ xBtn.addEventListener("click", function() {
 let wincount = 0;
 const win = document.getElementById("num");
 
+if (window.innerWidth < 500) {
 Playagain4.addEventListener("click", function() {
     document.querySelector(".box4").style.display = "none";
     document.getElementById("tag").style.display = "block";
     document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
 });
 
 Playagain5.addEventListener("click", function() {
     document.querySelector(".box5").style.display = "none";
     document.getElementById("tag").style.display = "block";
     document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
 });
 
 Playagain6.addEventListener("click", function() {
     document.querySelector(".box6").style.display = "none";
     document.getElementById("tag").style.display = "block";
     document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
 });
 
 Playagain7.addEventListener("click", function() {
     document.querySelector(".box7").style.display = "none";
     document.getElementById("tag").style.display = "block";
     document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+   
 });
 
 Playagain8.addEventListener("click", function() {
     document.querySelector(".box8").style.display = "none";
     document.getElementById("tag").style.display = "block";
     document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
 });
 
 
@@ -71,7 +87,9 @@ hand3.addEventListener('click', () => {
         document.querySelector(".box4").style.display = "block";
         document.getElementById("tag").style.display = "none";
         document.querySelector("#myRules").style.left="50%";
-
+        OR.style.display = "none";
+        Rule.style.left="57%";
+        Rule.style.top="90%";
 
         const Aichoice = document.getElementById("Aichoice1");
         let Hand = Math.ceil(Math.random()*5);
@@ -148,6 +166,9 @@ hand4.addEventListener('click', () => {
     document.querySelector(".box5").style.display = "block";
     document.getElementById("tag").style.display = "none";
     document.querySelector("#myRules").style.left="50%";
+    OR.style.display = "none";
+    Rule.style.left="57%";
+    Rule.style.top="90%";
     // 
     
     const Aichoice = document.getElementById("Aichoice2");
@@ -224,6 +245,9 @@ rock.addEventListener('click' ,() =>{
         document.querySelector(".box6").style.display = "block";
         document.getElementById("tag").style.display = "none";
         document.querySelector("#myRules").style.left="50%";
+        OR.style.display = "none";
+        Rule.style.left="57%";
+        Rule.style.top="90%";
 
         const Aichoice = document.getElementById("Aichoice3");
         let Hand = Math.ceil(Math.random()*5);
@@ -293,6 +317,9 @@ lizard.addEventListener('click', () => {
     document.querySelector(".box7").style.display = "block";
     document.getElementById("tag").style.display = "none";
     document.querySelector("#myRules").style.left="50%";
+    OR.style.display = "none";
+    Rule.style.left="57%";
+    Rule.style.top="90%";
 
     const Aichoice = document.getElementById("Aichoice4");
     let Hand = Math.ceil(Math.random()*5);
@@ -356,6 +383,9 @@ cross.addEventListener('click', () => {
     document.querySelector(".box8").style.display = "block";
     document.getElementById("tag").style.display = "none";
     document.querySelector("#myRules").style.left="50%";
+    OR.style.display = "none";
+    Rule.style.left="57%";
+    Rule.style.top="90%";
 
     const Aichoice = document.getElementById("Aichoice5");
     let Hand = Math.ceil(Math.random()*5);
@@ -419,6 +449,451 @@ if (wincount > 0) {
          }
 
 });
+}
+
+Playagain4.addEventListener("click", function() {
+    document.querySelector(".box4").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    // document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
+});
+
+Playagain5.addEventListener("click", function() {
+    document.querySelector(".box5").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    // document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
+});
+
+Playagain6.addEventListener("click", function() {
+    document.querySelector(".box6").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    // document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
+});
+
+Playagain7.addEventListener("click", function() {
+    document.querySelector(".box7").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    // document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+   
+});
+
+Playagain8.addEventListener("click", function() {
+    document.querySelector(".box8").style.display = "none";
+    document.getElementById("tag").style.display = "block";
+    // document.querySelector("#myRules").style.left="75%";
+    OR.style.display = "block";
+
+});
+
+
+hand3.addEventListener('click', () => {
+
+        document.querySelector(".box4").style.display = "block";
+        document.getElementById("tag").style.display = "none";
+      
+        OR.style.display = "none";
+      
+        const Aichoice = document.getElementById("Aichoice1");
+        let Hand = Math.ceil(Math.random()*5);
+    
+        if (Hand > 0 && Hand < 2) {
+                Aichoice.src = "img/rock.svg";
+        result.innerHTML = "You Win";
+        wincount++;
+        win.innerHTML = wincount;
+
+        setTimeout(function() {
+            oval.style.display = "flex";
+        }, 1000);
+        oval2.style.display = "none";
+    }
+ else if (Hand > 1 && Hand < 3) {
+    Aichoice.src = "img/hand.svg";
+    result.innerHTML = "Tie";
+    oval.style.display = "none";
+    oval2.style.display = "none";}
+
+    else if (Hand > 2 && Hand < 4) {
+        Aichoice.src = "cross.svg";
+        result.innerHTML = "You Win";
+
+        setTimeout(function() {
+            oval.style.display = "flex";
+        }, 1000);
+        oval2.style.display = "none";
+
+            wincount++;
+            win.textContent = wincount;
+
+    }
+
+    else if (Hand > 3 && Hand < 5) {
+        Aichoice.src = "img/hand2.svg";
+        result.innerHTML = "You Lose";
+
+        setTimeout(function() {
+            oval2.style.display = "flex";
+        }, 1000);
+
+        oval.style.display = "none";
+
+        if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+    }
+    else if (Hand > 4 && Hand < 6) {
+        Aichoice.src = "lizard.svg";
+        result.innerHTML = "You Lose";
+
+        setTimeout(function() {
+            oval2.style.display = "flex";
+        }, 1000);
+
+        oval.style.display = "none";
+
+        if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+    }
+    
+
+
+
+
+});
+
+hand4.addEventListener('click', () => {
+    document.querySelector(".box5").style.display = "block";
+    document.getElementById("tag").style.display = "none";
+    OR.style.display = "none";
+     const Aichoice = document.getElementById("Aichoice2");
+    let Hand = Math.ceil(Math.random()*5);
+
+    if (Hand > 0 && Hand < 2) {
+        Aichoice.src = "img/rock.svg";
+        result2.innerHTML = "You Lose";
+       
+        if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+        oval3.style.display = "none";
+
+        setTimeout(function() {
+                oval4.style.display = "flex";
+        }, 1000);
+    }
+    else if (Hand > 1 && Hand < 3) {
+            Aichoice.src = "img/hand.svg";
+        result2.innerHTML = "You Win";
+
+            wincount++;
+            win.textContent = wincount;
+
+       setTimeout(function() {
+            oval3.style.display = "flex";
+        }, 1000);
+        oval4.style.display = "none";
+    }
+
+        else if (Hand > 2 && Hand < 4) {
+            Aichoice.src = "cross.svg";
+            result2.innerHTML = "You Lose";
+
+            oval3.style.display = "none";
+            setTimeout(function() {
+                oval4.style.display = "flex";
+            }, 1000);
+    
+            if (wincount > 0) {
+                wincount--;
+                win.textContent = wincount;
+            }
+        }
+
+  else if (Hand > 3 && Hand < 5) {
+        Aichoice.src = "img/hand2.svg";
+        result2.innerHTML = "Tie"
+
+    oval3.style.display = "none";
+    oval4.style.display = "none";}
+
+ else if (Hand > 4 && Hand < 6) {
+        Aichoice.src = "lizard.svg";
+        result2.innerHTML = "You Win";
+
+        setTimeout(function() {
+            oval3.style.display = "flex";
+        }, 1000);
+        oval4.style.display = "none";
+
+            wincount++;
+            win.textContent = wincount;
+
+        }
+
+
+})
+
+rock.addEventListener('click' ,() =>{
+    
+        document.querySelector(".box6").style.display = "block";
+        document.getElementById("tag").style.display = "none";
+       OR.style.display = "none";
+       const Aichoice = document.getElementById("Aichoice3");
+        let Hand = Math.ceil(Math.random()*5);
+    
+        if (Hand > 0 && Hand < 2) {
+                Aichoice.src = "img/rock.svg";
+        result3.innerHTML = "Tie";
+    oval5.style.display = "none";
+    oval6.style.display = "none";
+    }
+    else if (Hand > 1 && Hand < 3) {
+        Aichoice.src = "img/hand.svg";
+        result3.innerHTML = "You Lose";
+        oval5.style.display = "none";
+        setTimeout(function() {
+        oval6.style.display = "flex";
+    }, 1000);
+     if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+        }
+    
+        else if (Hand > 2 && Hand < 4) {
+                Aichoice.src = "cross.svg";
+        result3.innerHTML = "You Lose";
+        setTimeout(function() {
+            oval6.style.display = "flex";
+        }, 1000);
+        oval5.style.display = "none";
+    
+
+        if (wincount > 0) {
+            wincount--;
+            win.textContent = wincount;
+        }
+        }
+
+    else if (Hand > 3 && Hand < 5) {
+        Aichoice.src = "img/hand2.svg";
+        result3.innerHTML = "You Win";
+
+        setTimeout(function() {
+                oval5.style.display = "flex";
+        }, 1000);
+       oval6.style.display = "none";
+        wincount++;
+            win.textContent = wincount; 
+        }
+        else if (Hand > 4 && Hand < 6) {
+            Aichoice.src = "lizard.svg";
+            result3.innerHTML = "You Win";
+    
+            setTimeout(function() {
+                oval5.style.display = "flex";
+            }, 1000);
+    
+            oval6.style.display = "none";
+                wincount++;
+                win.textContent = wincount;
+
+        }
+
+})
+
+lizard.addEventListener('click', () => {
+    document.querySelector(".box7").style.display = "block";
+    document.getElementById("tag").style.display = "none";
+   
+    OR.style.display = "none";
+    // Rule.style.left="57%";
+    // Rule.style.top="90%";
+
+    const Aichoice = document.getElementById("Aichoice4");
+    let Hand = Math.ceil(Math.random()*5);
+
+    if (Hand > 0 && Hand < 2) {
+        Aichoice.src = "img/rock.svg";
+        result4.innerHTML = "You Lose";
+    oval7.style.display = "none";
+
+    oval8.style.display = "flex";
+    if (wincount > 0) {
+        wincount--;
+        win.textContent = wincount;
+    }
+
+    }
+    else if (Hand > 1 && Hand < 3) {
+        Aichoice.src = "img/hand.svg";
+        result4.innerHTML = "You Win";
+        oval8.style.display = "none";
+        setTimeout(function() {
+            oval7.style.display = "flex";
+    }, 1000);
+        wincount++;
+        win.textContent = wincount;
+     }
+
+    else if (Hand > 2 && Hand < 4) {
+        Aichoice.src = "cross.svg";
+        result4.innerHTML = "You Win";
+        setTimeout(function() {
+                oval7.style.display = "flex";
+            }, 1000);
+            oval8.style.display = "none";
+               wincount++;
+                win.textContent = wincount;
+     }  
+     else if (Hand > 3 && Hand < 5) {
+            Aichoice.src = "img/hand2.svg";
+            result4.innerHTML = "You Lose";
+    
+            setTimeout(function() {
+                    oval8.style.display = "flex";
+        }, 1000);
+       oval7.style.display = "none";
+
+        if (wincount > 0) {
+                wincount--;
+            win.textContent = wincount;
+        }
+    }
+    else if (Hand > 4 && Hand < 6) {
+        Aichoice.src = "lizard.svg";
+        result4.innerHTML = "Tie";
+            oval7.style.display = "none";
+            oval8.style.display = "none";
+     }
+});
+
+cross.addEventListener('click', () => {
+    document.querySelector(".box8").style.display = "block";
+    document.getElementById("tag").style.display = "none";
+    // document.querySelector("#myRules").style.left="50%";
+    OR.style.display = "none";
+    // Rule.style.left="57%";
+    // Rule.style.top="90%";
+
+    const Aichoice = document.getElementById("Aichoice5");
+    let Hand = Math.ceil(Math.random()*5);
+
+    if (Hand > 0 && Hand < 2) {
+        Aichoice.src = "img/rock.svg";
+        result5.innerHTML = "You Win";
+    oval9.style.display = "none";
+    setTimeout(function() {
+        oval10.style.display = "flex";
+}, 1000);
+       wincount++;
+        win.textContent = wincount;
+ }
+
+ else if (Hand > 1 && Hand < 3) {
+    Aichoice.src = "img/hand.svg";
+    result5.innerHTML = "You Lose";
+    oval9.style.display = "none";
+    setTimeout(function() {
+    oval10.style.display = "flex";
+}, 1000);
+
+if (wincount > 0) {
+    wincount--;
+    win.textContent = wincount;
+}
+ }
+ else if (Hand > 2 && Hand < 4) {
+            Aichoice.src = "cross.svg";
+        result5.innerHTML = "Tie";
+ 
+            oval9.style.display = "none";
+            oval10.style.display = "none";
+     }  
+     else if (Hand > 3 && Hand < 5) {
+            Aichoice.src = "img/hand2.svg";
+        result5.innerHTML = "You Win";
+
+        setTimeout(function() {
+            oval9.style.display = "flex";
+        }, 1000);
+       oval10.style.display = "none";
+            wincount++;
+            win.textContent = wincount;
+         }
+         else if (Hand > 4 && Hand < 6) {
+            Aichoice.src = "lizard.svg";
+            
+            result5.innerHTML = "You Lose";
+                oval9.style.display = "none";
+
+                setTimeout(function() {
+                oval10.style.display = "flex";
+                }, 1000);
+
+                if (wincount > 0) {
+                    wincount--;
+                    win.textContent = wincount;
+                  }
+         }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // // Element Variables
